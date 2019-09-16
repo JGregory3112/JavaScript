bdfc0840c4630c117d9b64425a8ecd6fd34b587f
JGregory3112 <55250927+JGregory3112@users.noreply.github.com>
2019-09-17 00:22:54 +0100

    Add files via upload

---
+++ namescript.js
@@ -0,0 +1,6 @@
+var firstname = prompt("What is your first name? ");
+var lastname = prompt("What is your last name? ");
+var age = prompt("What is your age? ");
+
+alert("Your full name is " + firstname + " " + lastname)
+alert("You are " + age + " years old")
